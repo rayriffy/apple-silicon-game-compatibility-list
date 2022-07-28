c7a3dfc9ec8dc3d3979a4af3111c78c86468a5e1
Phumrapee Limpianchop <git@rayriffy.com>
2022-07-28 22:11:56 +0900

    feat: intitial c

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx,svelte,html}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
